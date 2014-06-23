27790bb14bc5cb024a01cf43c0ee74f4c0d5b433
jett <jett@atlantabike.org>
2014-06-22 22:44:48 -0400

    OPEN - issue VISCAT-149: Need start/end markers for the routes that I can turn on/off http://jira.jettmarks.com/jira/browse/VISCAT-149

--- showTrains/src/main/java/com/jettmarks/routes/client/ui/MarkerFactory.java
+++ showTrains/src/main/java/com/jettmarks/routes/client/ui/MarkerFactory.java
@@ -19,76 +19,77 @@
  */
 package com.jettmarks.routes.client.ui;
 
-import javax.swing.Icon;
-
 import com.google.gwt.maps.client.base.LatLng;
 import com.google.gwt.maps.client.base.Point;
-import com.google.gwt.maps.client.base.Size;
 import com.google.gwt.maps.client.overlays.Marker;
 import com.google.gwt.maps.client.overlays.MarkerImage;
 import com.google.gwt.maps.client.overlays.MarkerOptions;
 
 /**
  * Factors out the construction of specific markers by an enumerated type.
- *
+ * 
  * @author jett
  */
-public class MarkerFactory
-{
-  public enum MarkerType {START_MARKER, END_MARKER, SNAP_MARKER};
-  
-  private static MarkerImage beginIcon = MarkerImage.newInstance("images/dd-start.png");
-  private static MarkerImage endIcon = MarkerImage.newInstance("images/dd-end.png");
-  private static MarkerImage crosshairIcon = MarkerImage.newInstance("images/squareCrosshair.png");
-  
-  private static MarkerOptions beginOptions;
-  private static MarkerOptions endOptions;
-  private static MarkerOptions crosshairOptions;
-  
-  static
-  {
-//	  beginIcon.setIconAnchor(Point.newInstance(10, 32));
-    beginIcon.setAnchor(Point.newInstance(10, 32));
-	  beginOptions = MarkerOptions.newInstance();
-	  beginOptions.setIcon(beginIcon);
-	  
-//	  endIcon.setIconAnchor(Point.newInstance(10, 32));
-    endIcon.setAnchor(Point.newInstance(10, 32));
-	  endOptions = MarkerOptions.newInstance();
-	  endOptions.setIcon(endIcon);
-	  
-//	  int[] mapArray =
-//		  { 0, 0, 32, 0, 32, 32, 0, 32 };
-//	  crosshairIcon.setImageMap(mapArray);
-////	  crosshairIcon.setIconAnchor(Point.newInstance(16, 16));
-//	  crosshairIcon.setIconSize(Size.newInstance(32, 32));
-//	  crosshairIcon.setShadowURL(null);
-//	  crosshairOptions = MarkerOptions.newInstance(crosshairIcon);
-  }
-  
-  /**
-   * Creates an instance of a Marker with the properties defined statically in 
-   * this Class.
-   * 
-   * @param markerType
-   * @param point
-   * @return - Marker with the desired characteristics.
-   */
-  public static Marker getInstance(MarkerType markerType, LatLng point)
-  {
-    switch (markerType)
-    {
-    case START_MARKER:
-      beginOptions.setPosition(point);
-      return Marker.newInstance(beginOptions);
-	  case END_MARKER:
-	    endOptions.setPosition(point);
-      return Marker.newInstance(endOptions);
-    case SNAP_MARKER:
-//      return new Marker(point, crosshairOptions);
-    default:
-      return null;
-    }
-  }
-  
+public class MarkerFactory {
+	public enum MarkerType {
+		START_MARKER, END_MARKER, SNAP_MARKER
+	};
+
+	private static MarkerImage beginIcon = MarkerImage
+			.newInstance("images/dd-start.png");
+	private static MarkerImage endIcon = MarkerImage
+			.newInstance("images/dd-end.png");
+//	private static MarkerImage crosshairIcon = MarkerImage
+//			.newInstance("images/squareCrosshair.png");
+
+	private static MarkerOptions beginOptions;
+	private static MarkerOptions endOptions;
+//	private static MarkerOptions crosshairOptions;
+
+	static {
+		beginIcon.setAnchor(Point.newInstance(10, 32));
+		beginOptions = MarkerOptions.newInstance();
+		beginOptions.setIcon(beginIcon);
+
+		endIcon.setAnchor(Point.newInstance(10, 32));
+		endOptions = MarkerOptions.newInstance();
+		endOptions.setIcon(endIcon);
+
+		// int[] mapArray =
+		// { 0, 0, 32, 0, 32, 32, 0, 32 };
+		// crosshairIcon.setImageMap(mapArray);
+		// // crosshairIcon.setIconAnchor(Point.newInstance(16, 16));
+		// crosshairIcon.setIconSize(Size.newInstance(32, 32));
+		// crosshairIcon.setShadowURL(null);
+		// crosshairOptions = MarkerOptions.newInstance(crosshairIcon);
+	}
+
+	/**
+	 * Creates an instance of a Marker with the properties defined statically in
+	 * this Class.
+	 * 
+	 * @param markerType
+	 * @param point
+	 * @return - Marker with the desired characteristics.
+	 */
+	public static Marker getInstance(MarkerType markerType, LatLng point) {
+		Marker marker = null;
+		switch (markerType) {
+		case START_MARKER:
+			beginOptions.setPosition(point);
+			marker = Marker.newInstance(beginOptions);
+			marker.setIcon(beginIcon);
+			break;
+		case END_MARKER:
+			endOptions.setPosition(point);
+			marker = Marker.newInstance(endOptions);
+			marker.setIcon(endIcon);
+			break;
+		case SNAP_MARKER:
+			// return new Marker(point, crosshairOptions);
+		default:
+		}
+		return marker;
+	}
+
 }
