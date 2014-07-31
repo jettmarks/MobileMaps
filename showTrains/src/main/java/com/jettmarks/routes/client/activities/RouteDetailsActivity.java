d18b1a3570c9ef3240eb8a64019da0bea8b40963
jett <jettmarks@bellsouth.net>
2014-07-31 19:12:22 -0400

    RESOLVED - issue VISCAT-193: Quick Fix - User Privacy http://jira.jettmarks.com/jira/browse/VISCAT-193

--- showTrains/src/main/java/com/jettmarks/routes/client/activities/RouteDetailsActivity.java
+++ showTrains/src/main/java/com/jettmarks/routes/client/activities/RouteDetailsActivity.java
@@ -30,6 +30,7 @@ import com.jettmarks.routes.client.bean.BikeTrainRoute;
 import com.jettmarks.routes.client.bean.DisplayGroupDTO;
 import com.jettmarks.routes.client.place.EventPlace;
 import com.jettmarks.routes.client.place.RouteDetailsPlace;
+import com.jettmarks.routes.client.rep.RouteContainer;
 import com.jettmarks.routes.client.rep.RouteContainerFactory;
 import com.jettmarks.routes.client.ui.RouteDetailsView;
 
@@ -38,120 +39,116 @@ import com.jettmarks.routes.client.ui.RouteDetailsView;
  * 
  * @author jett
  */
-public class RouteDetailsActivity extends DetailActivity implements Activity
-{
-  private ClientFactory clientFactory = null;
+public class RouteDetailsActivity extends DetailActivity implements Activity {
+    private ClientFactory clientFactory = null;
 
-  private BikeTrainRoute bikeTrainRoute = null;
+    private BikeTrainRoute bikeTrainRoute = null;
 
-  private String headerText = null;
+    private String headerText = null;
 
-  /**
-   * @param detailView
-   * @param eventId
-   */
-  public RouteDetailsActivity(Place newPlace, ClientFactory cf)
-  {
-    super(cf.getRouteDetailsView(), "");
-    clientFactory = cf;
+    /**
+     * @param detailView
+     * @param eventId
+     */
+    public RouteDetailsActivity(Place newPlace, ClientFactory cf) {
+	super(cf.getRouteDetailsView(), "");
+	clientFactory = cf;
 
-    if (newPlace instanceof RouteDetailsPlace)
-    {
-      RouteDetailsPlace place = (RouteDetailsPlace) newPlace;
-      RouteDetailsView routeDetailsView = clientFactory.getRouteDetailsView();
-      bikeTrainRoute = place.getRoute();
-      if (bikeTrainRoute == null)
-      {
-        headerText = "Select a Route";
-      }
-      else
-      {
-        headerText = bikeTrainRoute.getDisplayName();
-      }
-      routeDetailsView.getMainButtonText().setText(headerText);
+	if (newPlace instanceof RouteDetailsPlace) {
+	    RouteDetailsPlace place = (RouteDetailsPlace) newPlace;
+	    RouteDetailsView routeDetailsView = clientFactory
+		    .getRouteDetailsView();
+	    bikeTrainRoute = place.getRoute();
+	    if (bikeTrainRoute == null) {
+		headerText = "Select a Route";
+	    } else {
+		headerText = bikeTrainRoute.getDisplayName();
+	    }
+	    routeDetailsView.getMainButtonText().setText(headerText);
 
-      addHandlerRegistration(routeDetailsView.getBackbutton().addTapHandler(
-          new TapHandler()
-          {
-            @Override
-            public void onTap(TapEvent event)
-            {
-              DisplayGroupDTO displayGroup = RouteContainerFactory.getRouteContainer()
-                                                                  .getCurrentDisplayGroup();
-              String displayName = displayGroup.getDisplayName();
-              String description = displayGroup.getDescription();
-              clientFactory.getPlaceController().goTo(
-                  new EventPlace(displayName, description));
-            }
-          }));
+	    addHandlerRegistration(routeDetailsView.getBackbutton()
+		    .addTapHandler(new TapHandler() {
+			@Override
+			public void onTap(TapEvent event) {
+			    DisplayGroupDTO displayGroup = RouteContainerFactory
+				    .getRouteContainer()
+				    .getCurrentDisplayGroup();
+			    String displayName = displayGroup.getDisplayName();
+			    String description = displayGroup.getDescription();
+			    clientFactory.getPlaceController().goTo(
+				    new EventPlace(displayName, description));
+			}
+		    }));
+	}
     }
-  }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see com.google.gwt.activity.shared.Activity#mayStop()
-   */
-  @Override
-  public String mayStop()
-  {
-    // TODO Auto-generated method stub
-    return null;
-  }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.google.gwt.activity.shared.Activity#mayStop()
+     */
+    @Override
+    public String mayStop() {
+	// TODO Auto-generated method stub
+	return null;
+    }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see com.google.gwt.activity.shared.Activity#onCancel()
-   */
-  @Override
-  public void onCancel()
-  {
-    // TODO Auto-generated method stub
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.google.gwt.activity.shared.Activity#onCancel()
+     */
+    @Override
+    public void onCancel() {
+	// TODO Auto-generated method stub
 
-  }
+    }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see com.google.gwt.activity.shared.Activity#onStop()
-   */
-  @Override
-  public void onStop()
-  {
-    cancelAllHandlerRegistrations();
-  }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.google.gwt.activity.shared.Activity#onStop()
+     */
+    @Override
+    public void onStop() {
+	cancelAllHandlerRegistrations();
+    }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see
-   * com.google.gwt.activity.shared.Activity#start(com.google.gwt.user.client
-   * .ui.AcceptsOneWidget, com.google.gwt.event.shared.EventBus)
-   */
-  @Override
-  public void start(AcceptsOneWidget panel, EventBus eventBus)
-  {
-    super.start(panel, eventBus);
-    final RouteDetailsView view = clientFactory.getRouteDetailsView();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.google.gwt.activity.shared.Activity#start(com.google.gwt.user.client
+     * .ui.AcceptsOneWidget, com.google.gwt.event.shared.EventBus)
+     */
+    @Override
+    public void start(AcceptsOneWidget panel, EventBus eventBus) {
+	super.start(panel, eventBus);
+	final RouteDetailsView view = clientFactory.getRouteDetailsView();
 
-    view.getBackbuttonText().setText("<");
-    view.getHeader().setText(headerText);
+	view.getBackbuttonText().setText("<");
+	view.getHeader().setText(headerText);
 
-    // Populate View with the BikeTrain
-    BikeTrainDTO bikeTrain = bikeTrainRoute.getBikeTrain();
-    view.setArrival(bikeTrain.getArrivalTime());
-    view.setDeparture(bikeTrain.getDepartureTime());
-    view.setLeaderName(bikeTrain.getLeaderName());
-    view.setLeaderEmail(bikeTrain.getLeaderEmail());
-    view.setLeaderPhone(bikeTrain.getLeaderPhone());
-    view.setNotes(bikeTrain.getNotes());
-    view.setRouteName(bikeTrain.getRouteName());
+	// Populate View with the BikeTrain
+	BikeTrainDTO bikeTrain = bikeTrainRoute.getBikeTrain();
+	view.setArrival(bikeTrain.getArrivalTime());
+	view.setDeparture(bikeTrain.getDepartureTime());
+	view.setLeaderName(bikeTrain.getLeaderName());
+	RouteContainer rc = RouteContainerFactory.getRouteContainer();
+	if (rc.getCurrentDisplayGroup().getDisplayName().equals("bt1408-Aug")) {
+	    view.setLeaderEmail(bikeTrain.getLeaderEmail());
+	    view.setLeaderPhone(bikeTrain.getLeaderPhone());
+	} else {
+	    view.setLeaderEmail("private");
+	    view.setLeaderPhone("private");
+	}
+	view.setNotes(bikeTrain.getNotes());
+	view.setRouteName(bikeTrain.getRouteName());
 
-    // Populate View with the BikeTrainRoute
-    view.setDisplayName(bikeTrainRoute.getDisplayName());
+	// Populate View with the BikeTrainRoute
+	view.setDisplayName(bikeTrainRoute.getDisplayName());
 
-    panel.setWidget(view);
-  }
+	panel.setWidget(view);
+    }
 
 }
