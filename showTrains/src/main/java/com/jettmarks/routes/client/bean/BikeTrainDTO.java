9f680a92988c3d41c02f968924b05738088e298b
a8l8f <a8l8f@NSCORP.COM>
2014-09-03 17:14:39 -0400

    OPEN - issue VISCAT-203: Refactoring/Comments/Cleanup Sept 2014 http://jira.jettmarks.com/jira/browse/VISCAT-203

--- showTrains/src/main/java/com/jettmarks/routes/client/bean/BikeTrainDTO.java
+++ showTrains/src/main/java/com/jettmarks/routes/client/bean/BikeTrainDTO.java
@@ -20,181 +20,175 @@ package com.jettmarks.routes.client.bean;
 import java.io.Serializable;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
-import com.jettmarks.routes.server.bean.BikeTrain;
 
 /**
- * Description.
- *
+ * Data Transfer Object for Bike Trains.
+ * 
  * @author jett
  */
-public class BikeTrainDTO implements Serializable, IsSerializable
-{
+public class BikeTrainDTO implements Serializable, IsSerializable {
 
-	
-/**
- * 
- */
-public BikeTrainDTO() {
-	// TODO Auto-generated constructor stub
-}
+	/**
+	 * 
+	 */
+	public BikeTrainDTO() {
+	}
 
-/**
-   * @return the id
-   */
-  public Integer getId()
-  {
-    return id;
-  }
-  /** *  */
-  private static final long serialVersionUID = -4583005121192909435L;
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
 
-  Integer id;
-  
-  private String routeName = null;
-  private String leaderName = null;
-  private String leaderEmail = null;
-  private String leaderPhone = null;
-  
-  private String departureTime = null;
-  private String arrivalTime = null;
-  private String notes = null;
-  
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#getLeaderName()
-   */
-  public String getLeaderName()
-  {
-    return leaderName;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#setLeaderName(java.lang.String)
-   */
-  public void setLeaderName(String leaderName)
-  {
-    this.leaderName = leaderName;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#getLeaderEmail()
-   */
-  public String getLeaderEmail()
-  {
-    return leaderEmail;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#setLeaderEmail(java.lang.String)
-   */
-  public void setLeaderEmail(String leaderEmail)
-  {
-    this.leaderEmail = leaderEmail;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#getLeaderPhone()
-   */
-  public String getLeaderPhone()
-  {
-    return leaderPhone;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#setLeaderPhone(java.lang.String)
-   */
-  public void setLeaderPhone(String leaderPhone)
-  {
-    this.leaderPhone = leaderPhone;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#getDepartureTime()
-   */
-  public String getDepartureTime()
-  {
-    return departureTime;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#setDepartureTime(java.lang.String)
-   */
-  public void setDepartureTime(String departureTime)
-  {
-    this.departureTime = departureTime;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#getArrivalTime()
-   */
-  public String getArrivalTime()
-  {
-    return arrivalTime;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#setArrivalTime(java.lang.String)
-   */
-  public void setArrivalTime(String arrivalTime)
-  {
-    this.arrivalTime = arrivalTime;
-  }
-  /**
-   * @param routeName
-   * @return
-   */
-  public static BikeTrainDTO getInstance(String routeName)
-  {
-    BikeTrainDTO newInstance;
-    if ("Sample-Bike-Train".equals(routeName))
-    {
-      newInstance = new BikeTrainDTO();
-	    newInstance.setArrivalTime("7:40 AM");
-	    newInstance.setDepartureTime("7:00 AM");
-	    newInstance.setLeaderEmail("dummy.email@nowhere.com");
-	    newInstance.setLeaderName("David Byrne");
-    }
-    else if (routeName.startsWith("Emory-to-Coke"))
-    {
-      newInstance = new BikeTrainDTO();
-	    newInstance.setArrivalTime("8:00 AM");
-	    newInstance.setDepartureTime("7:30 AM");
-	    newInstance.setLeaderEmail("mike@atlantabike.org");
-	    newInstance.setLeaderName("Michael Selik");
-	    newInstance.setNotes("No ride if it's raining.");
-    }
-    else
-    {
-      newInstance = null;
-	  }
-    return newInstance;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#getNotes()
-   */
-  public String getNotes()
-  {
-    return notes;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#setNotes(java.lang.String)
-   */
-  public void setNotes(String notes)
-  {
-    this.notes = notes;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#getRouteName()
-   */
-  public String getRouteName()
-  {
-    return routeName;
-  }
-  /* (non-Javadoc)
-   * @see com.jettmarks.routes.bean.IBikeTrain#setRouteName(java.lang.String)
-   */
-  public void setRouteName(String routeName)
-  {
-    this.routeName = routeName;
-  }
-  
-  /**
-   * For remembering that this instance already has an Id.
-   * @param id2
-   */
-  public void setId(Integer id2)
-  {
-    this.id = id2;
-  }
+	/** * */
+	private static final long serialVersionUID = -4583005121192909435L;
+
+	Integer id;
+
+	private String routeName = null;
+	private String leaderName = null;
+	private String leaderEmail = null;
+	private String leaderPhone = null;
+
+	private String departureTime = null;
+	private String arrivalTime = null;
+	private String notes = null;
+
+	/** 
+	 * @see com.jettmarks.routes.bean.IBikeTrain#getLeaderName()
+	 */
+	public String getLeaderName() {
+		return leaderName;
+	}
+
+	/** 
+	 * @see com.jettmarks.routes.bean.IBikeTrain#setLeaderName(java.lang.String)
+	 */
+	public void setLeaderName(String leaderName) {
+		this.leaderName = leaderName;
+	}
+
+	/** 
+	 * @see com.jettmarks.routes.bean.IBikeTrain#getLeaderEmail()
+	 */
+	public String getLeaderEmail() {
+		return leaderEmail;
+	}
+
+	/** 
+	 * @see
+	 * com.jettmarks.routes.bean.IBikeTrain#setLeaderEmail(java.lang.String)
+	 */
+	public void setLeaderEmail(String leaderEmail) {
+		this.leaderEmail = leaderEmail;
+	}
+
+	/** 
+	 * @see com.jettmarks.routes.bean.IBikeTrain#getLeaderPhone()
+	 */
+	public String getLeaderPhone() {
+		return leaderPhone;
+	}
+
+	/** 
+	 * @see
+	 * com.jettmarks.routes.bean.IBikeTrain#setLeaderPhone(java.lang.String)
+	 */
+	public void setLeaderPhone(String leaderPhone) {
+		this.leaderPhone = leaderPhone;
+	}
+
+	/** 
+	 * @see com.jettmarks.routes.bean.IBikeTrain#getDepartureTime()
+	 */
+	public String getDepartureTime() {
+		return departureTime;
+	}
+
+	/** 
+	 * @see
+	 * com.jettmarks.routes.bean.IBikeTrain#setDepartureTime(java.lang.String)
+	 */
+	public void setDepartureTime(String departureTime) {
+		this.departureTime = departureTime;
+	}
+
+	/** 
+	 * @see com.jettmarks.routes.bean.IBikeTrain#getArrivalTime()
+	 */
+	public String getArrivalTime() {
+		return arrivalTime;
+	}
+
+	/** 
+	 * @see
+	 * com.jettmarks.routes.bean.IBikeTrain#setArrivalTime(java.lang.String)
+	 */
+	public void setArrivalTime(String arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+
+	/**
+	 * @param routeName
+	 * @return
+	 */
+	public static BikeTrainDTO getInstance(String routeName) {
+		BikeTrainDTO newInstance;
+		if ("Sample-Bike-Train".equals(routeName)) {
+			newInstance = new BikeTrainDTO();
+			newInstance.setArrivalTime("7:40 AM");
+			newInstance.setDepartureTime("7:00 AM");
+			newInstance.setLeaderEmail("dummy.email@nowhere.com");
+			newInstance.setLeaderName("David Byrne");
+		} else if (routeName.startsWith("Emory-to-Coke")) {
+			newInstance = new BikeTrainDTO();
+			newInstance.setArrivalTime("8:00 AM");
+			newInstance.setDepartureTime("7:30 AM");
+			newInstance.setLeaderEmail("mike@atlantabike.org");
+			newInstance.setLeaderName("Michael Selik");
+			newInstance.setNotes("No ride if it's raining.");
+		} else {
+			newInstance = null;
+		}
+		return newInstance;
+	}
+
+	/** 
+	 * @see com.jettmarks.routes.bean.IBikeTrain#getNotes()
+	 */
+	public String getNotes() {
+		return notes;
+	}
+
+	/** 
+	 * @see com.jettmarks.routes.bean.IBikeTrain#setNotes(java.lang.String)
+	 */
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	/** 
+	 * @see com.jettmarks.routes.bean.IBikeTrain#getRouteName()
+	 */
+	public String getRouteName() {
+		return routeName;
+	}
+
+	/** 
+	 * @see com.jettmarks.routes.bean.IBikeTrain#setRouteName(java.lang.String)
+	 */
+	public void setRouteName(String routeName) {
+		this.routeName = routeName;
+	}
+
+	/**
+	 * For remembering that this instance already has an Id.
+	 * 
+	 * @param id2
+	 */
+	public void setId(Integer id2) {
+		this.id = id2;
+	}
 
 }
