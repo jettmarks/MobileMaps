95b808d70332132a18115df7f391271595fd6cfb
jett <jettmarks@gmail.com>
2015-04-05 11:09:44 -0400

    Added Home Button

--- showTrains/src/main/java/com/jettmarks/routes/client/css/AppBundle.java
+++ showTrains/src/main/java/com/jettmarks/routes/client/css/AppBundle.java
@@ -7,25 +7,25 @@ import com.google.gwt.resources.client.TextResource;
 import com.jettmarks.routes.client.ui.HeaderButtonBarCss;
 
 public interface AppBundle extends ClientBundle {
-    // This is a very nasty workaround because GWT CssResource does not support
-    // @media correctly!
-    @Source("app.css")
-    TextResource css();
+	// This is a very nasty workaround because GWT CssResource does not support
+	// @media correctly!
+	@Source("app.css")
+	TextResource css();
 
-    public static final AppBundle INSTANCE = GWT.create(AppBundle.class);
+	public static final AppBundle INSTANCE = GWT.create(AppBundle.class);
 
-    @Source("list.png")
-    ImageResource tabBarListImage();
+	@Source("list.png")
+	ImageResource tabBarListImage();
 
-    @Source("map.png")
-    ImageResource tabBarMapImage();
+	@Source("map.png")
+	ImageResource tabBarMapImage();
 
-    @Source("headerButtonBar.css")
-    HeaderButtonBarCss headerButtonBarCss();
+	@Source("headerButtonBar.css")
+	HeaderButtonBarCss headerButtonBarCss();
 
-    /**
-     * @return
-     */
-    @Source("home.png")
-    ImageResource getButtonBarHomeImage();
+	/**
+	 * @return
+	 */
+	@Source("home.png")
+	ImageResource getButtonBarHomeImage();
 }
